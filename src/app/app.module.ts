ad267fb515a80c85a2e8f7b304b49d9739658793
Alyssa Bryant <abryant@thisisfusion.com>
2017-10-18 19:06:55 -0500

    updates based on tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,49 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
 
+import { HeroDetailComponent } from './components/hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './services/hero.service';
+
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+      BrowserModule,
+      FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+      RouterModule.forRoot([
+        {
+          path: '',
+          redirectTo: '/dashboard',
+          pathMatch: 'full'
+        },
+        {
+          path: 'heroes',
+          component: HeroesComponent
+        },
+        {
+          path: 'dashboard',
+          component: DashboardComponent
+        },
+        {
+          path: 'detail/:id',
+          component: HeroDetailComponent
+        }
+      ])
+   ],
+  declarations: [
+                  AppComponent,
+                  HeroesComponent,
+                  HeroDetailComponent,
+                  DashboardComponent
+   ],
+  providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
+
 export class AppModule { }
---
+++ src/app/repositories/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from '../models/hero';
+
+export const HEROES:
+                    Hero[] = [
+                      { id: 11, name: 'Supergirl' },
+                      { id: 12, name: 'Lois Lane' },
+                      { id: 13, name: 'Wonder Woman' },
+                      { id: 14, name: 'Chloe Sullivan' },
+                      { id: 15, name: 'Harley Quinn' },
+                      { id: 16, name: 'The Tick' },
+                      { id: 17, name: 'Arthur' },
+                      { id: 18, name: 'Captain America' },
+                      { id: 19, name: 'Agent Carter' },
+                      { id: 20, name: 'Batman' }
+                    ];
